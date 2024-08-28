bf7886010f38bb0d05fc57f8b97b1e1e070318e6
yoonkyung <kyung3098@naver.com>
2024-08-28 15:54:02 +0900

    ➕ convex database 설치

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
+
+const Page = () => {
+  // const tasks = useQuery(api.tasks.get)
+  return <p>hi</p>
+}
+
+export default Page
