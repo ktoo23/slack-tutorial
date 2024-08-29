cdbafcb4f2c7668e33731c43b61959306689fe3b
yoonkyung <kyung3098@naver.com>
2024-08-29 17:19:17 +0900

    🚧 회원가입 컴포넌트에 name 필드 추가

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -24,6 +24,7 @@ interface SignUpCardProps {
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
   const { signIn } = useAuthActions();
 
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -39,7 +40,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
     }
 
     setPending(true);
-    signIn('password', { email, password, flow: 'signUp' })
+    signIn('password', { name, email, password, flow: 'signUp' })
       .catch(() => {
         setError('잘못된 요청입니다.');
       })
@@ -71,6 +72,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
       )}
       <CardContent className="space-y-5 px-0 pb-0">
         <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+          <Input
+            disabled={pending}
+            value={name}
+            placeholder="name"
+            type="Full name"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
           <Input
             disabled={pending}
             value={email}
