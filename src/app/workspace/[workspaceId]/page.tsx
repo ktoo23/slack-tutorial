c757da92a7e0f4912c8b431616edf287c370aa56
yoonkyung <kyung3098@naver.com>
2024-08-30 21:21:38 +0900

    🚧 툴바 컴포넌트 생성

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,12 @@
-interface WorkSpaceIdProps {
-  params: {
-    workspaceId: string;
-  };
-}
-const WorkSpaceIdPage = ({ params }: WorkSpaceIdProps) => {
-  const { workspaceId } = params;
-  return <div>ID: {workspaceId}</div>;
+'use client';
+
+import { useGetWorkSpace } from '@/features/workspaces/api/use-get-workspace';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
+const WorkSpaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkSpace({ id: workspaceId });
+  return <div>ID: {JSON.stringify(data)}</div>;
 };
 
 export default WorkSpaceIdPage;
