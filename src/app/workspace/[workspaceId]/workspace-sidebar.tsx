0ac3d3071d8db8a39f89233db8256cf2768d5ae3
yoonkyung <kyung3098@naver.com>
2024-09-04 13:19:28 +0900

    🚧 채널 페이지 생성

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -15,9 +15,11 @@ import { WorkspaceSection } from './workspace-section';
 import { useGetMembers } from '@/features/members/api/use-get-member';
 import { UserItem } from './user-item';
 import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal';
+import { useChannelId } from '@/hooks/use-channel-id';
 
 export const WorksapceSidebar = () => {
   const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
 
   const [_open, setOpen] = useCreateChannelModal();
 
@@ -78,6 +80,7 @@ export const WorksapceSidebar = () => {
             icon={HashIcon}
             label={item.name}
             id={item._id}
+            variant={channelId === item._id ? 'active' : 'default'}
           />
         ))}
       </WorkspaceSection>
--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -12,8 +12,11 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useCreateChannel } from '../api/use-create-channel';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 export const CreateChannelModal = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
 
   const [open, setOpen] = useCreateChannelModal();
@@ -38,7 +41,12 @@ export const CreateChannelModal = () => {
       {
         onSuccess: (id) => {
           // TODO: Redirect to new channel
+          toast.success('Success create channel');
+          router.push(`/workspace/${workspaceId}/channel/${id}`);
           handleClose();
+        },
+        onError: () => {
+          toast.error('Failed to create channel');
         },
       },
     );
