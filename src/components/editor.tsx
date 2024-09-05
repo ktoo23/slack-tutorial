1bcef3522a01930cb90166f7a34ef76d1e4fe4fb
yoonkyung <kyung3098@naver.com>
2024-09-05 14:35:25 +0900

    🚧 Emoji popover 창 생성 & 이모지 글쓰기

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -15,6 +15,7 @@ import { ImageIcon, Smile } from 'lucide-react';
 import { Hint } from './hint';
 import { Delta, Op } from 'quill/core';
 import { cn } from '@/lib/utils';
+import { EmojiPopover } from './emoji-popover';
 
 type EditorValue = {
   image: File | null;
@@ -140,6 +141,12 @@ const Editor = ({
   const isEmpty = text.replace(/<(.|\n)*?>/g, '').trim().length === 0;
 
   console.log({ isEmpty, text });
+
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
@@ -157,11 +164,11 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
             <Button disabled={disabled} size="iconSm" variant="ghost">
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           {variant === 'create' && (
             <Hint label="Image">
               <Button disabled={disabled} size="iconSm" variant="ghost">
@@ -205,11 +212,18 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] flex text-muted-foreground justify-end">
-        <p>
-          <strong>Shift + Return</strong>to add a new line
-        </p>
-      </div>
+      {variant === 'create' && (
+        <div
+          className={cn(
+            'p-2 text-[10px] flex text-muted-foreground justify-end opacity-0 transition',
+            !isEmpty && 'opacity-100',
+          )}
+        >
+          <p>
+            <strong>Shift + Return</strong>to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
